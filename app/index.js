c9ed95531c265db9b515bdf34199936e8dc3ab75
igor1289 <igor1289@gmail.com>
2024-07-06 17:27:05 +0300

    15.2

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5174'
+    origin: '*'
   }
 })
 
@@ -21,8 +21,26 @@ app.listen(HTTP_PORT, async () => {
   console.log('Server started')
 })
 
+const greetingMessage = {
+  text: 'Добро пожаловать в чат-попугай! <br> Он будет повторять всё что вы напишите<br><br>/exit - отключиться'
+}
+
+const farewellMessage = {
+  text: ":'(",
+  img: 'sad.gif'
+}
+
 io.on('connection', (socket) => {
-  console.log(`Подключен клиент`, socket)
+  socket.emit('message', greetingMessage)
+
+  socket.on('message', (message) => {
+    if (message != '/exit') {
+      socket.emit('message', { text: message })
+    } else {
+      socket.emit('message', farewellMessage)
+      socket.disconnect()
+    }
+  })
 })
 
 httpServer.listen(SOCKET_PORT)
--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,80 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import { io } from 'socket.io-client'
-
-const socket = io('http://localhost:3001')
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="layout container mt-4">
+    <div class="messages is-flex is-flex-direction-column is-justify-content-flex-end">
+      <div
+        class="is-flex mt-1 mb-1"
+        :class="{ 'is-justify-content-flex-end': !message.incoming }"
+        v-for="message in messages"
+        :key="message.id"
+      >
+        <div class="box">
+          <div v-html="message.text"></div>
+          <div v-if="message.img"><img :src="message.img" /></div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="message-input">
+      <textarea
+        class="textarea has-fixed-size"
+        placeholder="Введите сообщение"
+        rows="3"
+        v-model="messageText"
+      ></textarea>
+      <button class="button is-link mt-2" @click="sendMessage">Отправить</button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { io } from 'socket.io-client'
+import { ref } from 'vue'
+
+const messages = ref([])
+const messageText = ref('')
+
+const socket = io('http://localhost:3001')
+
+function addMessage(message, incoming = true) {
+  const newMessage = Object.assign({}, message)
+  newMessage.id = messages.value.length + 1
+  newMessage.incoming = incoming
+  messages.value.push(newMessage)
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+socket.on('message', (message) => {
+  addMessage(message)
+})
+
+socket.on('disconnect', (reason) => {
+  addMessage({ text: 'Оффлайн' }, true)
+})
+
+function sendMessage() {
+  if (messageText.value) {
+    socket.emit('message', messageText.value)
+
+    addMessage({ text: messageText.value }, false)
+
+    messageText.value = ''
+  }
+}
+</script>
+<style>
+@import 'bulma/css/bulma.min.css';
+
+.layout {
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr 200px;
+  grid-column-gap: 0px;
+  grid-row-gap: 5px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.messages {
+  grid-row: 1;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.message-input {
+  grid-row: 2;
 }
 </style>
